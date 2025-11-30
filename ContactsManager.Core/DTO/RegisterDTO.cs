1d24866791cc9aa1ad0640983c45838fc7c1ca00
ManfordZeng-waikato <zengchang389@gmail.com>
2025-11-30 15:54:10 +1300

    Add-feature Register View

---
+++ ContactsManager.Core/DTO/RegisterDTO.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ContactsManager.Core.DTO
+{
+    public class RegisterDTO
+    {
+        [Required(ErrorMessage = "Nmae cant't be blank")]
+        public string PersonName { get; set; }
+
+        [Required(ErrorMessage = "Email cant't be blank")]
+        [EmailAddress(ErrorMessage = "Email should be in a proper email address format")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "Phone number cant't be blank")]
+        [RegularExpression("^[0-9]*$", ErrorMessage = "'Phone number should contain numbers only")]
+        [DataType(DataType.PhoneNumber)]
+        public string Phone { get; set; }
+
+        [Required(ErrorMessage = "Password cant't be blank")]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        [DataType(DataType.Password)]
+        [Required(ErrorMessage = "Confirm Password cant't be blank")]
+        public string ConfirmPassword { get; set; }
+    }
+}
