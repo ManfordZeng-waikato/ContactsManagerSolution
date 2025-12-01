ddeabac55d39627cc6a176813219a303f0336cbd
ManfordZeng-waikato <zengchang389@gmail.com>
2025-12-01 18:14:10 +1300

    Feature-Password Comlexity Configuration

--- ContactsManager.UI/StartupExtensions/ConfigureServicesExtension.cs
+++ ContactsManager.UI/StartupExtensions/ConfigureServicesExtension.cs
@@ -56,7 +56,17 @@ namespace ContactsManager.UI.StartupExtensions
             }
             services.AddScoped<ResponseHeaderActionFilter>();
 
-            services.AddIdentity<ApplicationUser, ApplicationRole>()
+            services.AddIdentity<ApplicationUser, ApplicationRole>(options =>
+            {
+                options.Password.RequireDigit = true;
+                options.Password.RequireLowercase = true;
+                options.Password.RequireUppercase = true;
+                options.Password.RequireNonAlphanumeric = true;
+                options.Password.RequiredLength = 8;
+                options.Password.RequiredUniqueChars = 3;
+                options.User.RequireUniqueEmail = true;
+                options.SignIn.RequireConfirmedEmail = false;
+            })
                 .AddEntityFrameworkStores<ApplicationDbContext>()
                 .AddDefaultTokenProviders()
                 .AddUserStore<UserStore<ApplicationUser, ApplicationRole, ApplicationDbContext, Guid>>()
